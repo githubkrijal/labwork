1e244c92b620fccb0b1cb135e391d83b0ed3563e
Admin <krijalshrestha32@gmail.com>
2021-02-23 11:34:16 +0545

    labwork2

---
+++ labworl2.py
@@ -0,0 +1,6 @@
+#write a program that reads thr lenght of the base and the height of right angled triangle and print the area.
+#every number is given in separate line
+base=float(input("Enter the length of base"))
+height=float(input("Enter a height"))
+area=1/2*base*height
+print("The area is",area)
