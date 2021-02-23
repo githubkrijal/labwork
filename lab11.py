04b41009fe2648edb4606b376b82b0fb82449f49
Admin <krijalshrestha32@gmail.com>
2021-02-23 11:20:14 +0545

    Initial commit

---
+++ lab11.py
@@ -0,0 +1,6 @@
+#writee a program that takes thress nummbers and print their sum. Every number is given on a sepratee line
+number=int(input("Enter first number"))
+number2=int(input("Enter second number"))
+number3=int(input("Enter third number"))
+sum=number+number2+number3
+print("The number is",sum)
