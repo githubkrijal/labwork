ebb25cf1c2e2a4b60f8cd7045ec5889a8a5f2bd4
Admin <krijalshrestha32@gmail.com>
2021-02-23 11:57:03 +0545

    labwork3

---
+++ labb.py
@@ -0,0 +1,3 @@
+name=input("what is your name?")
+age=int(input("your age"))
+print("My name is" + name + " and my age is",age)
---
+++ labwork3.py
@@ -0,0 +1,8 @@
+#N student takes K apples and distribute thm among each other evenly.
+#the undivisible part remains
+apples=int(input("Enter the numbers of apples"))
+people=int(input("Entter the number of poeple"))
+basket=apples//people
+inbasket=apples%people
+print("distributed apple", basket)
+print("Remaining in basket",inbasket)
