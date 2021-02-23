a3a068cd023eb2211387cc1d8d28dd578a8b497c
Admin <krijalshrestha32@gmail.com>
2021-02-23 12:22:28 +0545

    labwork3

--- labwork3.py
+++ labwork3.py
@@ -2,7 +2,7 @@
 #the undivisible part remains
 apples=int(input("Enter the numbers of apples"))
 people=int(input("Entter the number of poeple"))
-basket=apples/people
-inbasket=apples%people
+basket=apples//people
+inbasket = apples % people
 print("distributed apple to each person", basket)
 print("Remaining in basket",inbasket)
